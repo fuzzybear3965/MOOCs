769b0dad28a9fc9f934385b5725cd264f225d7d3
Victoria Hoy <victoriahoy@Victorias-MacBook-Pro.local>
2015-02-01 09:59:19 -0500

    First version to compile successfully. I don't have the right solution.

--- Machine Problems/MP3_Basic_MMM.c
+++ Machine Problems/MP3_Basic_MMM.c
@@ -15,11 +15,12 @@ __global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
                                int numAColumns, int numBRows, int numBColumns,
                                int numCRows, int numCColumns) {
   //@@ Insert code to implement matrix multiplication here
+  int Row; int Col;
   Row = threadIdx.y+blockIdx.y*blockDim.y;
   Col = threadIdx.x+blockIdx.x*blockDim.x;
   if( Row < numARows && Col < numBColumns ) {
-    for(i = 0; i < numAColumns ; ++i) {
-       C[Row][Column] += A[Row][i]*B[i][Col];
+    for(int i = 0; i < numAColumns ; ++i) {
+       C[Row*numAColumns+Col] += A[Row*numAColumns+i]*B[i*numBColumns+Col];
     }
   }
 }
@@ -38,6 +39,9 @@ int main(int argc, char **argv) {
   int numBColumns; // number of columns in the matrix B
   int numCRows;    // number of rows in the matrix C (you have to set this)
   int numCColumns; // number of columns in the matrix C (you have to set this)
+ // int sizeofA;
+ // int sizeofB;
+  int sizeofC;
 
   args = wbArg_read(argc, argv);
 
@@ -51,35 +55,34 @@ int main(int argc, char **argv) {
   numCColumns = numBRows;
 
   // Defined below are the memory sizes of A, B and C, respectively
-  sizeofA = sizeof(float)*numARows*numAColumns;
-  sizeofB = sizeof(float)*numBRows*numBColumns;
+  // sizeofA = sizeof(float)*numARows*numAColumns;
+  // sizeofB = sizeof(float)*numBRows*numBColumns;
   sizeofC = sizeof(float)*numCRows*numCColumns;
 
   //@@ Allocate the hostC matrix
-  wbcheck(malloc(sizeofC));
+  malloc(sizeofC);
   wbTime_stop(Generic, "Importing data and creating memory on host");
 
   wbLog(TRACE, "The dimensions of A are ", numARows, " x ", numAColumns);
   wbLog(TRACE, "The dimensions of B are ", numBRows, " x ", numBColumns);
 
-  wbLog(TRACE, “The size of A, in memory, is: %d”, sizeof(float)*numARows*numAColumns)
   wbTime_start(GPU, "Allocating GPU memory.");
   //@@ Allocate GPU memory here
-  wbCheck(cudaMalloc((void**)&deviceA,sizeof(float)*numARows*numAColumns);
-  wbCheck(cudaMalloc((void**)&deviceB,sizeof(float)*numARows*numAColumns);
-  wbCheck(cudaMalloc((void**)&deviceC,sizeof(float)*numCRows*numCColumns);
+  wbCheck(cudaMalloc((void**)&deviceA,sizeof(float)*numARows*numAColumns));
+  wbCheck(cudaMalloc((void**)&deviceB,sizeof(float)*numARows*numAColumns));
+  wbCheck(cudaMalloc((void**)&deviceC,sizeof(float)*numCRows*numCColumns));
   wbTime_stop(GPU, "Allocating GPU memory.");
 
   wbTime_start(GPU, "Copying input memory to the GPU.");
   //@@ Copy memory to the GPU here
-  wbCheck(cudaMemcpy(deviceA, hostA, sizeof(float)*numARows*numAColumns, cudaMemcpyHostToDevice);
-  wbCheck(cudaMemcpy(deviceB, hostB, sizeof(float)*numBRows*numBColumns, cudaMemcpyHostToDevice);
+  wbCheck(cudaMemcpy(deviceA, hostA, sizeof(float)*numARows*numAColumns, cudaMemcpyHostToDevice));
+  wbCheck(cudaMemcpy(deviceB, hostB, sizeof(float)*numBRows*numBColumns, cudaMemcpyHostToDevice));
 
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
 
-  dim3 griddim(ceiling(numCColumns/256.0),ceiling(numCRows/256.0),1);
+  dim3 griddim(ceil(numCColumns/256.0),ceil(numCRows/256.0),1);
   dim3 blockdim(256,256,1);
 
   wbTime_start(Compute, "Performing CUDA computation");
@@ -91,13 +94,13 @@ int main(int argc, char **argv) {
 
   wbTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
-  wbcheck(cudaMemcpy(hostC, deviceC, sizeof(float)*numCRows*numCColumns,cudaMemcpyDeviceToHost);
+  wbCheck(cudaMemcpy(hostC, deviceC, sizeof(float)*numCRows*numCColumns,cudaMemcpyDeviceToHost));
 
   wbTime_stop(Copy, "Copying output memory to the CPU");
 
   wbTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
-  wbcheck(cudaFree(deviceA)); wbcheck(cudaFree(deviceB)); wbcheck(cudaFree(deviceC));
+  wbCheck(cudaFree(deviceA)); wbCheck(cudaFree(deviceB)); wbCheck(cudaFree(deviceC));
 
   wbTime_stop(GPU, "Freeing GPU Memory");
 
